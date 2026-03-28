99d88ece2a500d1350c14cc3380541e7b6d56364
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-03-28 16:02:54 -0300

    Update exercise_compare.py

--- exercise_compare.py
+++ exercise_compare.py
@@ -19,7 +19,7 @@ def compare():
     numero1 = int(input())
     numero2 = int(input())
     if numero1 > numero2:
-        print(f"{numero1} es mayor que {3}")
+        print(f"{numero1} es mayor que {numero2}")
     elif numero1 < numero2:
         print(f"{numero1} es menor que {numero2}")
     elif numero1 == numero2:
