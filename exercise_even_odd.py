4867247fc95f9a6a1e575ac7a2532bda28e47d6d
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-03-28 16:24:41 -0300

    Update exercise_even_odd.py

--- exercise_even_odd.py
+++ exercise_even_odd.py
@@ -15,6 +15,6 @@ def even_odd():
     pass
     numero = int(input())
     if numero % 2 == 0:
-        print(f"EL numero {numero} es par")
+        print(f"El numero {numero} es par")
     else:
         print(f"El numero {numero} es impar")
